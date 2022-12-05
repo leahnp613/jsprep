3517fafc679f368d06d351f297a489af302a2218
leahnp613 <leahnagarpowers@gmail.com>
2022-12-05 14:56:06 -0500

    Playing with fumctions

---
+++ 8.Theonlickeventlistener/index.js
@@ -0,0 +1,13 @@
+//document.getElementByID("count-el").innerText = 5
+
+//let count = 5
+//count + 1
+//console.log(count)
+
+
+function increment(){
+    console.log("The button was clicked")
+}
+
+
+
---
+++ 9.Usingfunctionstowritelesscode/index.js
@@ -0,0 +1,21 @@
+//Setting up the race
+
+
+function countdown() {
+    console.log(5)
+    console.log(4)
+    console.log(3)
+    console.log(2)
+    console.log(1)
+
+}
+countdown()
+
+
+//G!
+//Players are running the race
+//Race is finished!
+
+//Get ready for a new race
+
+countdown()
