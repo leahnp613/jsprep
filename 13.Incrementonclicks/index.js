067d8d29c9bad1d3c41b7b500389b05c3784e5f7
leahnp613 <leahnagarpowers@gmail.com>
2022-12-05 18:33:24 -0500

    Incrementing on clicks

---
+++ 13.Incrementonclicks/index.js
@@ -0,0 +1,17 @@
+//document.getElementByID("count-el").innerText = 5
+
+//let count = 5
+//count = count +1
+//console.log(count)
+
+
+//intialze the count as 0
+//listen for licks on the increment button
+//increment the count variable when the button is clicked
+//change the count-el in the HTML to reflect the new count
+
+ let count = 0
+function increment() {
+    let count = count + 1
+    console.log(count)
+}
