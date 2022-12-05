cba01f3d30eb535ced1a589fb55bf44e1b127ec3
leahnp613 <leahnagarpowers@gmail.com>
2022-12-05 14:33:07 -0500

    Adding INCREMENT button

---
+++ 6.Reassigningandincrementing/index.js
@@ -0,0 +1,27 @@
+//document.getElementByID("count-el").innerText = 5
+
+let count = 5
+
+//count + 1
+
+count = count + 1
+
+count = count + 2
+
+console.log(count)
+
+let bonusPoints = 50
+console.log(bonusPoints)
+
+ let bonusPoints = bonusPoints +50
+console.log(bonusPoints)
+
+ let bonusPoints = bonusPoints -25
+console.log(bonusPoints)
+
+
+ let bonusPoints = 70
+
+console.log(bonusPoints)
+
+
