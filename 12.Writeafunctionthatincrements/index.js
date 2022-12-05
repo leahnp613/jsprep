eb827cc4d61caf87448eb55ed11936cab8fd0373
leahnp613 <leahnagarpowers@gmail.com>
2022-12-05 15:22:59 -0500

    Writing a function that increments

---
+++ 12.Writeafunctionthatincrements/index.js
@@ -0,0 +1,14 @@
+let lapsCompleted = 0
+
+//Create a function that increments the lapsCompleted variable with one
+//Run it three times
+
+function lapsRun() {
+    lapsCompleted = lapsCompleted + 1
+}
+lapsRun()
+lapsRun()
+lapsRun()
+
+
+console.log(lapsCompleted)
